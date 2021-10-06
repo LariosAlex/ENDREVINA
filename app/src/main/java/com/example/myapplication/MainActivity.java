42f5f960497486dc5a1cbf3727b15a76ed6604bd
Alex <alexlarios49@gmail.com>
2021-10-06 20:18:58 +0200

    Endrevina -- Miercoles 6 de Octubre

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,20 +20,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.submitButton);
+
+        EditText UserNum;
+        UserNum = (EditText) findViewById(R.id.sortNumber);
+        String textUserNum = UserNum.getText().toString();
+
+        int numRandom = (int) Math.floor(Math.random() * 99 + 1);
+        TextView Intents = (TextView) findViewById(R.id.intents);
+
         button.setOnClickListener(new View.OnClickListener() {
+            int intents = 0;
             public void onClick(View v) {
                 Context context = getApplicationContext();
-                int numRandom = (int) Math.floor(Math.random() * 99 + 1);
-                int numUser = sortNumber.getString();
-                if (numRandom < sortNumber) {
-                    CharSequence text = "El teu numero es mes gran";
+                CharSequence textNumber = "";
+                int numUser = Integer.parseInt(UserNum.getText().toString());
+                intents = intents + 1;
+                if (numRandom < numUser) {
+                    textNumber = "El teu numero es mes petit \n" + numRandom;
+                    UserNum.setText("");
+                    Intents.setText("");
+                    Intents.setText("Intents totals:" + intents);
+                }
+                else if (numRandom > numUser){
+                    textNumber = "El teu numero es mes gran \n" + numRandom;
+                    UserNum.setText("");
+                    Intents.setText("");
+                    Intents.setText("Intents totals:" + intents);
                 }
                 else{
-                    CharSequence text = "El teu numero es mes petit";
+                    textNumber = "El teu numero es correcte";
+                    Intent intent = new Intent (v.getContext(), Ranking.class);
+                    intent.putExtra("Pepe", intents);
+                    startActivity(intent);
                 }
                 int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
+                Toast toast = Toast.makeText(context, textNumber, duration);
                 toast.show();
             }
         });
